53f9e84a7de120986efde692a1cb676d976aae30
Poppy Dalton <poppycdalton@gmail.com>
2026-02-18 13:51:56 -0600

    Update step1-for-loop-input-search.js

--- scripts/step1-for-loop-input-search.js
+++ scripts/step1-for-loop-input-search.js
@@ -3,40 +3,45 @@ const searchResults = document.getElementById("searchResults");
 const searchMessage = document.getElementById("searchMessage");
 
 
+
+
 function renderMessage(msg) {
-  searchMessage.innerHTML = `<li>${msg}</li>`;
+ searchMessage.innerHTML = `<li>${msg}</li>`;
 }
 
+
 searchInput.addEventListener("input", function (event) {
-  // const query = event.target.value; 
-  // constquery = event.target.value.trim(); //remove whitespace
-  const query = event
-  .target
-  .value
-  .trim()
-  .toLowerCase();
+ // const query = event.target.value;
+ // constquery = event.target.value.trim(); //remove whitespace
+ const query = event
+ .target
+ .value
+ .trim()
+ .toLowerCase();
 
 
-  let found = false;
+ let found = false;
 
-  for (let i = 0; i < jsArrayData.length; i++) {
-    // if (jsArrayData[i] === query) {
-    // if (jsArrayData[i].toLowerCase() === query) { //if comparing lowercase only?
-    if (jsArrayData[i].toLowerCase().includes(query)) { // substring matches
 
-      found = true;
-      break;
-    }
-  }
-  
+ for (let i = 0; i < jsArrayData.length; i++) {
+   // if (jsArrayData[i] === query) {
+   // if (jsArrayData[i].toLowerCase() === query) { //if comparing lowercase only?
+   if (jsArrayData[i].toLowerCase().includes(query)) { // substring matches
+
+
+     found = true;
+     break;
+   }
+ }
   if (!query) {
-    renderMessage("Search items you are looking for (example: clothes)");
-  } else if (found) {
-    renderMessage(`Found exact match: ${query}`);
-  } else {
-    renderMessage(`No exact match: ${query}`);
-  }
+   renderMessage("Search items you are looking for (example: clothing)");
+ } else if (found) {
+   renderMessage(`Found substring match: ${query}`);
+ } else {
+   renderMessage(`No substring match: ${query}`);
+ }
 });
 
+
 // initial message
-renderMessage("Search items you are looking for (example: clothes)");
+renderMessage("Search items you are looking for (example: clothing or shoes)");
